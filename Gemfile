30556eef00233b67b084a83e766d6d35c7c6fbfa
Eduardo Turiño <eturino@artirix.com>
2017-04-10 20:16:12 +0100

    coveralls

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in dag_link_calculator.gemspec
 gemspec
+
+gem 'coveralls', '>= 0.8.20', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'bundler/setup'
+require 'pry'
+require 'simplecov'
+require 'coveralls'
+
+Coveralls.wear!
+
 require 'dag_link_calculator'
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
